9b27be19691ff3f91ee6e490a67bdacbdd8cd1f5
Kelly DeMent <kdement@ufl.edu>
2019-11-10 22:24:48 -0500

    Create desserts class

---
+++ src/main.js
@@ -0,0 +1,23 @@
+//Create Desserts class, pass properties called "type" and "calories"
+class Desserts {
+  constructor (type, calories) {
+    this.type = type;
+    this.calories = calories;
+  }
+}
+
+//Create subclass called IceCream that extends Desserts
+//Inside of IceCream, pass in two parent properties and two new properties called "flavor" and "scoops"
+
+//In the subclass IceCream, also include method called "includeSpoon"
+
+
+//In this method, log the string 'Here is your spoon!' to the console
+
+
+//At the bottom of your file, create a new instance of IceCream called vanillaIceCream passing in the values "Ice Cream", 340, "Vanilla", and 3
+
+//Console.log the new object vanillaIceCream
+//console.log(vanillaIceCream);
+
+//On a new line, call the includeSpoon() method on vanillaIceCream
